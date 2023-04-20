868bbbbef67d5b356e842382e881d831ac345aad
GalPerelman <peligur@gmail.com>
2023-04-20 20:05:45 +0300

    finalize lp optimization module

--- lp.py
+++ lp.py
@@ -1,10 +1,11 @@
-import os
-import pandas as pd
-import numpy as np
+import os.path
 
+import numpy as np
+import pandas as pd
 from rsome import ro
-import rsome as rso
 from rsome import grb_solver as grb
+
+import utils
 
 
 class LP:
@@ -21,6 +22,10 @@ class LP:
         self.range_constraints()
         self.one_comb_only()
         self.mass_balance()
+        self.vsp_initial_flow()
+        self.vsp_total_vol()
+        self.vsp_flow_change()
+        self.max_power()
         self.obj_function()
 
     def range_constraints(self):
@@ -38,49 +43,83 @@ class LP:
         self.model.min(obj_func)
 
     def one_comb_only(self):
-        facilities = self.sim.net.fsp['facility'].unique()
-        for i in range(len(facilities)):
-            mat = np.zeros(len(self.sim.net.fsp))
-            facility_idx = self.sim.net.fsp.loc[self.sim.net.fsp.facility == facilities[i]].index.values
-            mat[facility_idx] = 1
-            self.model.st((mat @ self.x_fsp).T <= np.ones((24, 1)))
+        for pump_station in self.sim.net.fsp['facility'].unique():
+            idx = self.sim.net.fsp.loc[self.sim.net.fsp['facility'] == pump_station].index.to_list()
+            self.model.st(sum([self.x_fsp[_, :] for _ in idx]) <= 1)
 
     def mass_balance(self):
-        for tank_idx, tank_row in self.sim.net.tanks.iterrows():
-            lhs = tank_row["init_vol"]
-            for j, fsp_row in self.sim.net.fsp.iterrows():
-                if fsp_row["in"] == tank_idx:
-                    mat = self.sim.net.get_cumulative_mat(self.sim.T, 1)
-                    flow = fsp_row['flow']
-                    lhs += flow * self.x_fsp[j, :] @ mat
-                elif fsp_row["out"] == tank_idx:
-                    mat = self.sim.net.get_cumulative_mat(self.sim.T, -1)
-                    flow = fsp_row['flow']
-                    lhs += flow * self.x_fsp[j, :] @ mat
-                else:
-                    continue
-
-            for j, vsp_row in self.sim.net.vsp.iterrows():
-                if vsp_row["in"] == tank_idx:
-                    mat = self.sim.net.get_cumulative_mat(self.sim.T, 1)
-                    lhs += self.x_vsp[j, :] @ mat
-                elif vsp_row["out"] == tank_idx:
-                    mat = self.sim.net.get_cumulative_mat(self.sim.T, -1)
-                    lhs += self.x_vsp[j, :] @ mat
-                else:
-                    continue
-
+        for tank_idx, row in self.sim.net.tanks.iterrows():
             tank_consumer = self.sim.net.tanks.loc[tank_idx, 'demand']
-            tank_demand = self.sim.data[tank_consumer] * 0.2
-            mat = self.sim.net.get_cumulative_mat(self.sim.T, 1)
-            cum_demand = mat @ tank_demand.values
-            min_vol_vector = self.sim.get_min_vol_vector(tank_idx, 1)
-            self.model.st(lhs >= min_vol_vector + cum_demand)
-            self.model.st(lhs <= self.sim.net.tanks.loc[tank_idx, "max_vol"] + cum_demand)
+            tank_demand = self.sim.data[tank_consumer].values
+            cum_tank_demand = self.sim.data[tank_consumer].values.cumsum()
+            init_vol = self.sim.net.tanks.loc[tank_idx, "init_vol"]
+            max_vol = self.sim.net.tanks.loc[tank_idx, "max_vol"]
+            min_vol = self.sim.net.tanks.loc[tank_idx, "min_vol"]
+            min_vol_vector = self.sim.get_min_vol_vector(tank_idx)
+
+            fsp_inflow_idx = self.sim.net.fsp.loc[self.sim.net.fsp['in'] == tank_idx].index.to_list()
+            fsp_outflow_idx = self.sim.net.fsp.loc[self.sim.net.fsp['out'] == tank_idx].index.to_list()
+            vsp_inflow_idx = self.sim.net.vsp.loc[self.sim.net.vsp['in'] == tank_idx].index.to_list()
+            vsp_outflow_idx = self.sim.net.vsp.loc[self.sim.net.vsp['out'] == tank_idx].index.to_list()
+
+            fsp_inflow = self.sim.net.fsp.loc[fsp_inflow_idx, 'flow'].values
+            fsp_outflow = self.sim.net.fsp.loc[fsp_outflow_idx, 'flow'].values
+            for t in range(self.sim.T):
+                lhs = init_vol
+                if fsp_inflow_idx:
+                    lhs += fsp_inflow @ (self.x_fsp[fsp_inflow_idx, :t + 1]).sum(axis=1)
+                if vsp_inflow_idx:
+                    lhs += (self.x_vsp[vsp_inflow_idx, :t + 1]).sum()
+                if fsp_outflow_idx:
+                    lhs -= fsp_outflow @ (self.x_fsp[fsp_outflow_idx, :t + 1]).sum(axis=1)
+                if vsp_outflow_idx:
+                    lhs -= (self.x_vsp[vsp_outflow_idx, :t + 1]).sum()
+
+                lhs = lhs - (tank_demand[:t + 1]).sum()
+                self.model.st(lhs >= min_vol_vector[t])
+                self.model.st(lhs <= max_vol)
+
+    def vsp_initial_flow(self):
+        for i, row in self.sim.net.vsp.iterrows():
+            if np.isnan(row['init_flow']):
+                continue
+            else:
+                self.model.st(self.x_vsp[i, 0] == row['init_flow'])
+
+    def vsp_total_vol(self):
+        for i, row in self.sim.net.vsp.iterrows():
+            self.model.st(self.x_vsp[i, :].sum() >= row['min_vol'])
+            self.model.st(self.x_vsp[i, :].sum() <= row['max_vol'])
+
+    def vsp_flow_change(self):
+        const_tariff = utils.get_constant_tariff_periods(self.sim.data['tariff']).astype(int)
+        for i, row in self.sim.net.vsp.iterrows():
+            if row['const_flow']:
+                for j in range(max(const_tariff) + 1):
+                    idx = np.where(const_tariff == j)[0]
+                    mat = utils.get_mat_for_vsp_value_changes(self.sim.T, idx)
+                    self.model.st(mat @ self.x_vsp[i, :].T == 0)
+            else:
+                continue
+
+    def max_power(self):
+        """
+        This function is currently customized for Sopron network only
+        The problem conditions are such that the only power constraint is
+        Power Station D (Pump Stations 5 and 6) must be under 35 kW during the On-Peak periods
+        The meaning is that Pump station 5 cannot be operated with 116 CMH (37.5 kW) during the ON-Peak periods
+        """
+        max_power_constr = pd.read_csv(os.path.join(self.sim.data_folder, 'max_power.csv'))
+        for i, row in max_power_constr.iterrows():
+            fsp_idx = self.sim.net.fsp.loc[self.sim.net.fsp['comb'] == row['comb']].index.values[0]
+            print(row['comb'])
+            print(fsp_idx)
+            mat = utils.get_mat_for_tariff(self.sim, tariff_name=row['tariff'])
+            self.model.st(mat @ self.x_fsp[fsp_idx, :] == 0)
 
     def solve(self):
         self.model.solve(solver=grb, display=False)
-        obj, x, status = self.model.solution.objval, self.model.solution.x, self.model.solution.status
-        print(status, obj)
-        print(len(x))
+        obj, status = self.model.solution.objval, self.model.solution.status
+        x_fsp_val, x_vsp_val = self.x_fsp.get(), self.x_vsp.get()
+        return obj, status, x_fsp_val, x_vsp_val
 
