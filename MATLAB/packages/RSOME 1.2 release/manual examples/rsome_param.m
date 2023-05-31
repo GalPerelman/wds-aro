a7413f6d360936acc2551c064a297bddc6533708
GalPerelman <peligur@gmail.com>
2023-05-31 10:54:53 +0300

    remove matlab models from main branch

--- MATLAB/packages/RSOME 1.2 release/manual examples/rsome_param.m
+++
@@ -1,9 +0,0 @@
-model = rsome;				   		   % Create an RSOME model
-
-model.Param.solver = 'gurobi';		   % Change the solver to be Gurobi
-model.Param.solver = 'cplex';		   % Change the solver to be CPLEX
-
-model.Param.display = 0;	           % Disable the display of solution status
-model.Param.display = 1;	           % Enable the display of solution status
-
-model.Param.mipgap = 1e-3;	           % Set the MIP gap to be 1e-3
